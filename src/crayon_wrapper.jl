8f5de9976de065c773b163393a4dd238585d7753
KristofferC <kcarlsson89@gmail.com>
2020-09-14 16:48:54 +0200

    add a show method for CrayonWrapper

--- src/crayon_wrapper.jl
+++ src/crayon_wrapper.jl
@@ -11,15 +11,25 @@ function (c::Crayon)(args::Union{CrayonWrapper,AbstractString}...)
 end
 
 Base.print(io::IO, cw::CrayonWrapper) = _print(io, cw, CrayonStack(incremental = true))
-
 _print(io::IO, str::String, stack::CrayonStack) = print(io, str)
-
 function _print(io::IO, cw::CrayonWrapper, stack::CrayonStack)
     print(io, push!(stack, cw.c))
     for obj in cw.v
         _print(io, obj, stack)
     end
     length(stack.crayons) > 1 && print(io, pop!(stack))
+    return
+end
+
+Base.show(io::IO, ::MIME"text/plain", cw::CrayonWrapper) = _show(io, cw, CrayonStack(incremental = true))
+_show(io::IO, str::String, stack::CrayonStack) = print(io, str)
+function _show(io::IO, cw::CrayonWrapper, stack::CrayonStack)
+    show(io, MIME("text/plain"), cw.c)
+    print(io, push!(stack, cw.c))
+    for obj in cw.v
+        _show(io, obj, stack)
+    end
+    length(stack.crayons) > 1 && show(io, MIME("text/plain"), pop!(stack))
     return
 end
 
