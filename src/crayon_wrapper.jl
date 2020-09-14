ac51ef0c40b15d0cefe5d378426f1385d0dde0a8
KristofferC <kcarlsson89@gmail.com>
2020-09-14 13:49:28 +0200

    correctly overload show instead of print

--- src/crayon_wrapper.jl
+++ src/crayon_wrapper.jl
@@ -10,14 +10,14 @@ function (c::Crayon)(args::Union{CrayonWrapper,AbstractString}...)
     CrayonWrapper(c, typefix.(collect(args)))
 end
 
-Base.show(io::IO, cw::CrayonWrapper) = _show(io, cw, CrayonStack(incremental = true))
+Base.print(io::IO, cw::CrayonWrapper) = _print(io, cw, CrayonStack(incremental = true))
 
-_show(io::IO, str::String, stack::CrayonStack) = print(io, str)
+_print(io::IO, str::String, stack::CrayonStack) = print(io, str)
 
-function _show(io::IO, cw::CrayonWrapper, stack::CrayonStack)
+function _print(io::IO, cw::CrayonWrapper, stack::CrayonStack)
     print(io, push!(stack, cw.c))
     for obj in cw.v
-        _show(io, obj, stack)
+        _print(io, obj, stack)
     end
     length(stack.crayons) > 1 && print(io, pop!(stack))
     return
